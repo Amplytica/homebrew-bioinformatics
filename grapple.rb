2caed04b9ec1309d339598525dcbedebd61c8c56
Quinton Sirianni <qsirianni@gmail.com>
2015-09-26 18:57:36 -0700

    Add Symbolic Link to Grapple Formula
    Added a symbolic link to the grapple formula so that grapple can be called without the .py extension

--- grapple.rb
+++ grapple.rb
@@ -29,6 +29,7 @@ class Grapple < Formula
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    system "ln -s grapple.py #{bin}/grapple"
   end
 
   test do
